13f2435f22574908cf9ca6ec7987040dc4f6a8b4
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2026-02-16 00:13:19 -0300

    feat: initial configuration

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/**/*.spec.ts',
+    '!src/**/*.test.ts'
+  ],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node']
+};
